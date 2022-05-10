d74b90028daecdb42fdead5ee5c10a5cbd4ca029
Simon Johansson <sijh21@student.bth.se>
2022-05-10 13:11:02 +0200

    Some comments and clean up

--- main.py
+++ main.py
@@ -14,10 +14,10 @@ import _thread
 # This program requires LEGO EV3 MicroPython v2.0 or higher.
 # Click "Open user guide" on the EV3 extension tab for more information.
 
-# Create your objects here.
+# Creatimg EV3Brick Object
 ev3 = EV3Brick()
 
-#Motor definitions
+# Motor definitions
 Left_drive = Motor(Port.C, positive_direction=Direction.COUNTERCLOCKWISE)
 Right_drive = Motor(Port.B, positive_direction=Direction.COUNTERCLOCKWISE)
 Crane_motor = Motor(Port.A)
@@ -27,40 +27,43 @@ touch_sensor = TouchSensor(Port.S1)
 colour_sensor = ColorSensor(Port.S3)
 ultrasonic_sensor = UltrasonicSensor(Port.S4)
 
+# Creating DriveBase
 robot = DriveBase(Left_drive, Right_drive, wheel_diameter=47, axle_track=128) #SB. Stämmer wheel och axle för roboten vi har just nu?
 
-# Constants
+# Constants line reflections
 LINE_REFLECTION = 67
 OFF_LINE_REFLECTION = 84
 
+# Calculating threshold
 threshold = (LINE_REFLECTION + OFF_LINE_REFLECTION) / 2
 
+# Speeds and distances
 DRIVE_SPEED = 75
 DRIVE_WITH_PALLET = 50
 CRANE_SPEED = 200
 STOP_DISTANCE = 350
 PALET_DISTANCE = 500
 
+# Turn rates
 DESIRED_TURN_RATE = 45
 TURN_RATE_AMPLIFIER = DESIRED_TURN_RATE / ((OFF_LINE_REFLECTION - LINE_REFLECTION) / 2)
 
+# Ground lift angle
 GROUND_LIFT_ANGLE = 50
 
+# Bool for driving with pallet
 driving_with_pallet = False
 
-#Colour
-brown_warehouse = Color.BROWN
-red_warehouse = Color.RED
-blue_warehouse = Color.BLUE
-green_to_pickup_and_deliver = Color.GREEN
-olive_for_center_circle = 0
-purple_in_deliver = 0
-
+# path color
 path_color = "red"
-current_location = None
+# current location
+current_location = "Somewhere"
+# clear load pallet
+clear_road = True
 
+# Color dictionary with all the rgb values
 COLORS = {
-    "red": (48,16,26),
+    "red": (32,16,26),
     "blue": (7,19,37),
     "yellow line": (39,35,10),
     "brown": (14,9,12),
@@ -71,44 +74,43 @@ COLORS = {
     "white": (46,55,97)
 }
 
+# All the colors of the lines to follow
 TMP_COLORS = COLORS.copy()
 for color in ["white", "brown", "black"]:
     del TMP_COLORS[color]
 LINE_COLORS = TMP_COLORS.keys()
 
-#Here is where you code starts
-
+# Calibrating colors
 def calibrate_colors(COLORS):
+    # Temporary COLORS dict to change
     temp_colors = COLORS.copy()
+    # Loops trough all the colors to update their values
     for key in temp_colors:
         print_on_screen("Select color " + key)
+        # Wait untill you press center button on ev3 to update the colors
         while True:
             if Button.CENTER in ev3.buttons.pressed():
                 temp_colors[key] = colour_sensor.rgb()
-                wait(1000)
+                wait(1000) # Wait between each press so that it wont double press and take two colors
                 break
-    ev3.screen.clear()
-                
-    return temp_colors
+    ev3.screen.clear() # Reset screen
+    wait(2000) # Wait two seconds before moving on so that you can move it
+    return temp_colors # Returns temp_colors
     
-def print_on_screen(text):
+    # print_on_screen, clears screen and prints new text
+def print_on_screen(text): 
     ev3.screen.clear()
     ev3.screen.print(str(text))
 
-def select_color():
-    color = input("Please select a color? ")
-    return color
-
-def print_on_screen(text):
-    ev3.screen.clear()
-    ev3.screen.print(str(text))
-
+    # Classify color
 def classify_color(rgb_in):
-    OFFSET = 13
-    match_r = []
+    OFFSET = 8 # Offset for each color value
+    match_r = [] 
     match_g = []
     match_b = []
     r_in, g_in, b_in = rgb_in
+    # Goes through every color and checks if each of the values are within the offset of the color value
+    # If they are whitin the offset they are added to the matches
     for color_key in COLORS.keys():
         r, g, b = COLORS[color_key]
         if r_in < (r + OFFSET) and r_in > (r - OFFSET):
@@ -118,14 +120,17 @@ def classify_color(rgb_in):
         if b_in < (b + OFFSET) and b_in > (b - OFFSET):
             match_b.append(color_key)
     matches = []
+    # Checks if all the color values are within the offset if so adds the color to matches
     for color_key in COLORS.keys():
         if color_key in match_r and color_key in match_g and color_key in match_b:
             matches.append(color_key)
+    # If no colors are detected return None
     if len(matches) == 0:
         return [None]
     # print(matches)
     return matches
 
+# Compars arrays returns True if there are matches in the arrays, False if there are None
 def compare_arrays(array_1, array_2):
     matches = 0
     for element1 in array_1:
@@ -136,8 +141,10 @@ def compare_arrays(array_1, array_2):
         return True
     return False
 
+# Searching for the correct path while driving along the cirkle
 def select_path():
-    # print_on_screen(f'Seachring for {path_color} path.')
+    global current_location
+    print_on_screen("Searching for " + path_color + " path.")
     """
     Antagande:
     Trucken befinner sig på den gula mutt-ringen.
@@ -146,16 +153,17 @@ def select_path():
     """
     while path_color not in classify_color(colour_sensor.rgb()):
         # print (classify_color(colour_sensor.rgb())[0])
-        if compare_arrays(classify_color(colour_sensor.rgb()), ["red", "blue", "green", "purple"]):
+        if compare_arrays(classify_color(colour_sensor.rgb()), ["red", "blue",]):
             robot.straight(50)
         else:
             follow_color()
     align_right()
     current_location = path_color
-    # print_on_screen(f'Found path to {current_location} warehouse.')
+    print_on_screen('Found path to ' + current_location +  ' warehouse.')
     
 def drive_to_destination():
-    # print_on_screen(f'Driving to {current_location} warehouse.')
+    global current_location
+    print_on_screen('Driving to ' + current_location + ' warehouse.')
     """
     Antagande:
     Trucken har hittat önskad väg och är justerad efter dess riktning.
@@ -165,10 +173,11 @@ def drive_to_destination():
     while "black" not in classify_color(colour_sensor.rgb()):
         follow_color()
     robot.drive(0, 0)
-    # print_on_screen(f'Arrived at {current_location} warehouse.')
+    print_on_screen('Arrived at ' + current_location + ' warehouse.')
 
 def return_to_circle():
-    # print_on_screen('Leaving {current_location} warehouse and returning to the middle circle.')
+    global current_location
+    print_on_screen('Leaving ' + current_location + ' warehouse and returning to the middle circle.')
     """
     Antagande:
     Trucken står på den svarta ytan i ett lager med nosen innåt i lagret.
@@ -183,11 +192,10 @@ def return_to_circle():
         else:
             follow_color()
     align_right()
-    # print_on_screen('Arrived at the middle circle.')
+    print_on_screen('Arrived at the middle circle.')
 
 def align_right():
-    robot.drive(0, -45)
-    wait(2500)
+    robot.turn(-120)
     robot.drive(0, 0)
 
 def drive_forward(precise = True) -> None:
@@ -197,7 +205,7 @@ def drive_forward(precise = True) -> None:
     if driving_with_pallet == True:
         drive_speed = 40
     if precise:
-        # speed = drive_speed / (0.8 + abs(deviation) * 0.06)
+        speed = drive_speed / (0.8 + abs(deviation) * 0.06)
         speed = drive_speed * max(0.1, 1 - (abs(turn_rate) / DESIRED_TURN_RATE))
     else:
         speed = drive_speed
@@ -205,18 +213,22 @@ def drive_forward(precise = True) -> None:
 
 
 def follow_color(color_array = LINE_COLORS):
-    drive_speed = 100
+    global clear_road
+    if(clear_road):
+        drive_speed = 100
     if driving_with_pallet == True:
-        drive_speed = 40
+        drive_speed = -40
     turn_rate = 35
     while compare_arrays(color_array, classify_color(colour_sensor.rgb())):
         turn_rate = 0
         drive_speed = 0
         robot.turn(-20)
         if compare_arrays(color_array, classify_color(colour_sensor.rgb())):
-            robot.turn(-45)
+            robot.turn(45)
             robot.straight(-70)
     robot.drive(drive_speed, turn_rate)
+    #else:
+    #    robot.drive(0,0)
 
 def find_pallet(is_pallet_on_ground: bool) -> None:
     print_on_screen('Searching for a pallet.')
@@ -227,11 +239,11 @@ def find_pallet(is_pallet_on_ground: bool) -> None:
     Vi är redo att köra pickup pallet, med eller utan höjd
     """
     while "yellow line" not in classify_color(colour_sensor.rgb()):
-        robot.drive(40,20)
+        robot.drive(40,-20)
     while ultrasonic_sensor.distance()> PALET_DISTANCE:
         follow_color(["yellow line"])
-    print("End my pain :)")
-    if (is_pallet_on_ground) :
+    print_on_screen("Found pallet")
+    if (is_pallet_on_ground):
         pick_up_pallet_on_ground()
     elif(is_pallet_on_ground == False ):#elif 
         #pick_up
@@ -264,9 +276,10 @@ def pick_up_pallet_on_ground() -> None:
     drive_forward_stop_time = time.perf_counter()
     robot.drive(0, 0)
     if not is_pallet_on_properly:
-        print("Picking up failed.")
+        print_on_screen("Picking up failed.")
     else:
-        print("picking up")
+        print_on_screen("picking up")
+        Crane_motor.reset_angle(0)
         Crane_motor.run_angle(CRANE_SPEED, GROUND_LIFT_ANGLE)
         driving_with_pallet = True
     time_to_back_out = drive_forward_stop_time -  drive_forward_time
@@ -276,20 +289,22 @@ def pick_up_pallet_on_ground() -> None:
     #Sväng om?
 def reset_crane():
     Crane_motor.run_until_stalled(CRANE_SPEED)
+    Crane_motor.run_until_stalled(-CRANE_SPEED)
 
 # Main thread for driving etc
 def main():
-    # while(True):
-    while ultrasonic_sensor.presence() == False and ultrasonic_sensor.distance() > STOP_DISTANCE:
+    while(True):
         select_path()
         drive_to_destination()
+        reset_crane()
         find_pallet(True)
-        # Get out of warehouse
+    #Get out of warehouse
         return_to_circle()
 
         
 def get_color():
     while (True):
+        global path_color
         color = input("Choose color... ")
         if str(color).lower() in COLORS.keys():
             path_color = color.lower()
@@ -298,10 +313,18 @@ def get_color():
         else:
             print("No color matching input")
 
+def collision_check():
+    while True:
+        global clear_road
+        if ultrasonic_sensor.distance() < STOP_DISTANCE:
+            clear_road = False
+        else:
+            clear_road = True
 # COLORS = calibrate_colors(COLORS)
 
+_thread.start_new_thread(collision_check,(),)
 _thread.start_new_thread(main,(),)
-# _thread.start_new_thread(get_color,(),)
+_thread.start_new_thread(get_color,(),)
 
 while True:
     pass
