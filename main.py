d63225d636b34bb4246a7a1a27a80c2fed660af6
Simon Johansson <sijh21@student.bth.se>
2022-05-20 14:41:56 +0200

    Doc strings and clean up

--- main.py
+++ main.py
@@ -34,8 +34,8 @@ robot = DriveBase(Left_drive, Right_drive, wheel_diameter=47, axle_track=128) #S
 
 # Driving variables
 DRIVE_SPEED = 200
-DRIVE_WITH_PALLET = 150
-TURN_RATE_AMPLIFIER = 1 # 1 för robot 8, 1.8 för robot 4
+DRIVE_WITH_PALLET = 100
+TURN_RATE_AMPLIFIER = 1.3 # 1 för robot 8, 1.8 för robot 4
 CRANE_SPEED = 200
 STOP_DISTANCE = 350
 PALET_DISTANCE = 500
@@ -44,11 +44,11 @@ GROUND_LIFT_ANGLE = 150
 # Global variables
 clear_road = True
 driving_with_pallet = False
-path_color = "red"
+selected_path_color = "red"
 current_location = "middle circle"
 emergency_mode = False
 
-# Color dictionary with all the rgb values
+# Standard rgb values for colors
 COLORS = {
     "red": (68,23,40),
     "blue": (7,19,37),
@@ -61,6 +61,7 @@ COLORS = {
     "white": (72,86,100)
 }
 
+# Different locations
 LOCATIONS = {
     "red": Color.RED,
     "blue": Color.BLUE,
@@ -71,52 +72,63 @@ LOCATIONS = {
     "blue warehouse": Color.BLUE
 }
 
-# All the colors of the lines to follow
 TMP_COLORS = COLORS.copy()
 for color in ["white", "brown", "black"]:
     del TMP_COLORS[color]
 LINE_COLORS = TMP_COLORS.keys()
 
-# Calibrating colors
 def calibrate_colors(COLORS):
-    # Temporary COLORS dict to change
+    """
+    Creates a dictionary with rgb values for each colors.
+    The function lets the user copy the rgb values for colors
+    by holding the color sensor over the requested color wich
+    shows up on the ev3 hub screen
+    """
     temp_colors = COLORS.copy()
-    # Loops trough all the colors to update their values
     for key in temp_colors:
         print_on_screen("Select color " + key)
-        # Wait untill you press center button on ev3 to update the colors
         while True:
             if Button.CENTER in ev3.buttons.pressed():
                 temp_colors[key] = colour_sensor.rgb()
-                wait(1000) # Wait between each press so that it wont double press and take two colors
+                wait(1000)
                 break
-    ev3.screen.clear() # Reset screen
-    wait(2000) # Wait two seconds before moving on so that you can move it
-    return temp_colors # Returns temp_colors
+    ev3.screen.clear()
+    wait(2000)
+    return temp_colors
     
-    # print_on_screen, clears screen and prints new text
 def open_file_colors(COLORS):
+    """
+    Opens or creates a new file that saves colors rgb values.
+    """
     with open('RGB.txt', 'w') as convert_file:
      convert_file.write(json.dumps(COLORS))
 
-def print_on_screen(text): 
+def print_on_screen(text):
+    """
+    Clears the screen on the ev3 hub and prints string input instead.
+    """
     ev3.screen.clear()
     ev3.screen.print(str(text))
 
 def change_color(color_key):
+    """
+    Changes the color of the light on the ev3 hub.
+    """
     ev3.light.on(LOCATIONS[color_key])
 
-    # Classify color
 def classify_color(rgb_in, offset = None):
-    OFFSET = 7 # Offset for each color value
+    """
+    Uses the rgb values from the color sensor to decide what color it
+    detects based on predetermined values of each colors rgb values
+    takes the rgb values aswell as an allowed offset as arguments
+    """
+    OFFSET = 4
     if offset is None:
         offset = OFFSET
     match_r = [] 
     match_g = []
     match_b = []
     r_in, g_in, b_in = rgb_in
-    # Goes through every color and checks if each of the values are within the offset of the color value
-    # If they are whitin the offset they are added to the matches
     for color_key in COLORS.keys():
         r, g, b = COLORS[color_key]
         if r_in < (r + offset) and r_in > (r - offset):
@@ -126,18 +138,18 @@ def classify_color(rgb_in, offset = None):
         if b_in < (b + offset) and b_in > (b - offset):
             match_b.append(color_key)
     matches = []
-    # Checks if all the color values are within the offset if so adds the color to matches
     for color_key in COLORS.keys():
         if color_key in match_r and color_key in match_g and color_key in match_b:
             matches.append(color_key)
-    # If no colors are detected return None
     if len(matches) == 0:
         return [None]
-    # print(matches)
     return matches
 
-# Compars arrays returns True if there are matches in the arrays, False if there are None
 def compare_arrays(array_1, array_2):
+    """
+    Compares to arrays and return True if the elements in
+    array 1 exists in array 2
+    """
     matches = 0
     for element1 in array_1:
         for element2 in array_2:
@@ -147,42 +159,39 @@ def compare_arrays(array_1, array_2):
         return True
     return False
 
-# Searching for the correct path while driving along the cirkle
 def select_path():
+    """
+    Drives around the middle circle util it finds the path that
+    corresponds with the global variable selected_path_color and skips the paths
+    that does not.
+    """
     global current_location
-    global path_color
+    global selected_path_color
     global driving_with_pallet
     ev3.light.on(Color.YELLOW)
-    print_on_screen("Searching for " + path_color + " path.")
-    """
-    Antagande:
-    Trucken befinner sig på den gula mutt-ringen.
-    Resultat:
-    Trucken hittar vägen av önskad färg och svänger mot den.
-    """
+    print_on_screen("Searching for " + selected_path_color + " path.")
+
     color = colour_sensor.rgb()
-    #print (classify_color(color))
-    while path_color not in classify_color(color):
+    while selected_path_color not in classify_color(color):
         if compare_arrays(classify_color(color), ["red", "blue" , "purple" , "green"]):
             robot.straight(70)
         else:
-            follow_line(color,COLORS["middle circle"])
+            follow_line(color)
         color = colour_sensor.rgb()
     print("I found the path! Are you proud? :)")
     if driving_with_pallet:
         align_right()
-    current_location = path_color
+    current_location = selected_path_color
     change_color(current_location)
     
 def drive_to_destination():
+    """
+    The robot drives from the middle circle to the warehouse when
+    the correct path is found untill it sees black which is the 
+    entry to each warehouse.
+    """
     global current_location
     print_on_screen('Driving to ' + current_location + ' warehouse.')
-    """
-    Antagande:
-    Trucken har hittat önskad väg och är justerad efter dess riktning.
-    Resultat:
-    Trucken kör fram till den svarta ytan där vägen möter varuhuset.
-    """
     color = colour_sensor.rgb()
     while colour_sensor.color() != Color.BLACK:
         follow_line(color)
@@ -191,42 +200,49 @@ def drive_to_destination():
     change_color(current_location)
 
 def return_to_circle():
+    """
+    The truck drives from the warehouse back to the circle by driving untill
+    it detects the color of the middlecircle.
+    """
     global current_location
     print_on_screen('Leaving ' + current_location + ' warehouse and returning to the middle circle.')
-    """
-    Antagande:
-    Trucken står på den svarta ytan i ett lager med nosen innåt i lagret.
-    Resultat:
-    Trucken svänger ut och kör tillbaka till mitt-cirkeln.
-    """
-    if current_location != 'blue warehouse':
-        robot.turn(300)
+    robot.turn(300)
     ev3.speaker.say("Leaving " +  current_location)
     color = colour_sensor.rgb()
-    while ("middle circle" not in classify_color(color,15)):
-        follow_line(color,COLORS[path_color])
+    while ("middle circle" not in classify_color(color,20)):
+        follow_line(color)
         color = colour_sensor.rgb()
     align_right()
     current_location = 'middle circle'
     change_color(current_location)
     print_on_screen('Arrived at the middle circle.')
     
-def return_to_area():
-    global path_color
+def return_to_area(location_override = None):
+    """
+    Depending on what area the robot is leaving this function changes
+    what path color the robot should look for and drives it back to the middle circle.
+    """
+    global selected_path_color
     global current_location
+    if location_override != None:
+        current_location = location_override
     if current_location == "middle circle":
-        path_color = "green"
+        selected_path_color = "green"
     elif current_location == "red warehouse":
-        path_color = "green"
+        selected_path_color = "green"
         return_to_circle()
     elif current_location == "blue warehouse":
-        path_color = "green"
+        selected_path_color = "green"
         return_to_circle()
     elif current_location == "pickup and delivery":
-        path_color = "red"
+        selected_path_color = "red"
         return_to_circle()
 
 def align_right():
+    """
+    Tells the robot to turn different amounts depending on if the robot
+    carries a pallet or not.
+    """
     global driving_with_pallet
     if driving_with_pallet:
         robot.straight(-100)
@@ -237,26 +253,23 @@ def align_right():
         robot.drive(0,0)
 
 def deviation_from_rgb(rgb_in, line_color):
-    w_r, w_g, w_b = COLORS['white']
-    l_r, l_g, l_b = line_color
-    threshold_r = (w_r + l_r) / 2
-    threshold_g = (w_g + l_g) / 2
-    threshold_b = (w_b + l_b) / 2
-    r_in, g_in, b_in = rgb_in
-    deviation_r = r_in - threshold_r
-    deviation_g = g_in - threshold_g
-    deviation_b = b_in - threshold_b
-    deviation = deviation_r + deviation_g + deviation_b 
+    """
+    Calculates the deviation of the current rgb values from the color sensor
+    to the average between white and the line the robot is supposed to follow.
+    """
+    sum_white = sum(COLORS['white'])
+    sum_line = sum(line_color)
+    threshold = (sum_white + sum_line) / 2
+    sum_in = sum(rgb_in)
+    deviation = sum_in - threshold
     return deviation
 
-    # sum_white = sum(COLORS['white'])
-    # sum_line = sum(line_color)
-    # threshold = (sum_white + sum_line) / 2
-    # sum_in = sum(rgb_in)
-    # deviation = sum_in - threshold
-    # return deviation
-
 def follow_line(rgb_in, line_color = COLORS['red']) -> None:
+    """
+    Follows the selected line, takes the rgb values of the color sensor and
+    what color to follow as arguments. The robot follows the line based of the
+    deviation from the function "deviation_from_rgb()".
+    """
     global clear_road
     global driving_with_pallet
     deviation_turn_offset = 1
@@ -274,7 +287,6 @@ def follow_line(rgb_in, line_color = COLORS['red']) -> None:
                 drive_speed = DRIVE_WITH_PALLET
             speed = drive_speed / (0.9 + abs(deviation) * 0.01)
             if (abs(deviation) + deviation_turn_offset >= threshold) and (deviation < 0):
-                # speed = -speed
                 robot.drive(0,0)
                 turn_rate = 0
                 drive_speed = 0
@@ -289,7 +301,11 @@ def follow_line(rgb_in, line_color = COLORS['red']) -> None:
             robot.drive(0,0)
 
 def follow_color(color_array = LINE_COLORS):
-    global clear_road
+    """
+    This function allows the robot to follow a line by checking if it sees the color of the
+    line, if it does it will turn right and then drive forward while going a bit left untill
+    it sees the color again. 
+    """
     drive_speed = 100
     if driving_with_pallet == True:
         drive_speed = 40
@@ -301,23 +317,22 @@ def follow_color(color_array = LINE_COLORS):
             robot.turn(-45)
             robot.straight(-70)
     robot.drive(drive_speed, turn_rate)
-    #else:
-    #    robot.drive(0,0)
 
 def find_pallet(is_pallet_on_ground: bool) -> None:
+    """
+    When entering a warehouse the robot drives untill it finds a yellow line,
+    left or right depending on what warehouse it is in. Then it checks if 
+    there is a pallet infront of it. If there is it runs the pickup pallet 
+    function. If there isn't it crosses the yellow line and drives to the next
+    line and checks there.
+    """
     print_on_screen('Searching for a pallet.')
-    """
-    Antagande:
-    Vi står i varuhuset
-    Resultat:
-    Vi är redo att köra pickup pallet, med eller utan höjd
-    """
     global current_location
     global driving_with_pallet
     pallet_position = 1
-    if current_location == 'red': # Handling pallets in brown warehouse
+    if current_location == 'red':
         current_location = "red warehouse"
-        if ultrasonic_sensor.distance() < PALET_DISTANCE + 150: # Handling pallets in first slot
+        if ultrasonic_sensor.distance() < PALET_DISTANCE + 150:
             pallet_position = 1
             while "yellow line" not in classify_color(colour_sensor.rgb()):
                 robot.drive(40,20)
@@ -327,7 +342,7 @@ def find_pallet(is_pallet_on_ground: bool) -> None:
             else:
                 pick_up_pallet_in_air(pallet_position)
 
-        else: # Handling pallets in second slot
+        else:
             pallet_position = 2
             while "yellow line" not in classify_color(colour_sensor.rgb()):
                 robot.drive(40,20)
@@ -340,9 +355,9 @@ def find_pallet(is_pallet_on_ground: bool) -> None:
             else:
                 pick_up_pallet_in_air(pallet_position)
 
-    elif current_location == 'blue': # Handling pallets in blue warehouse
+    elif current_location == 'blue':
         current_location = "blue warehouse"
-        if ultrasonic_sensor.distance() < PALET_DISTANCE + 150: # Handling pallets in second slot
+        if ultrasonic_sensor.distance() < PALET_DISTANCE + 150:
             pallet_position = 1
             while "yellow line" not in classify_color(colour_sensor.rgb()):
                 robot.drive(40, -20)
@@ -357,7 +372,7 @@ def find_pallet(is_pallet_on_ground: bool) -> None:
             while 'blue' not in classify_color(colour_sensor.rgb()):
                 robot.drive(-100, 0)
 
-        else: # Handling pallets in thrid slot
+        else:
             pallet_position = 2
             while "yellow line" not in classify_color(colour_sensor.rgb()):
                 robot.drive(40,-20)
@@ -380,23 +395,21 @@ def find_pallet(is_pallet_on_ground: bool) -> None:
         current_location = "pickup and delivery"
         wait(5000)
         driving_with_pallet = False
-        # Sväng vänster ställ ner pallet sväng tillbaka och kör ut igen
 
 def pick_up_pallet_in_air(pallet_position) -> None:
+    """
+    Runs if the pallet is elevated and sets the starting position of the crane
+    to an elevated position before running the pickup pallet function.
+    """
     Crane_motor.run_angle(CRANE_SPEED, 200)
     pick_up_pallet_on_ground(pallet_position)
     Crane_motor.run_angle(-CRANE_SPEED, 200)
 
 def pick_up_pallet_on_ground(pallet_position) -> None:
+    """
+    Drive forward and pickup the pallet then reverse back to the starting position
+    """
     print_on_screen('Picking up the found pallet.')
-    """
-    Antagande:
-    En pallet har redan hittats och 
-    Vi står med nosen pekande på den
-    Resultat:
-    Palleten är upplockad
-    Vi har backat tillbacka till start position. 
-    """
     global driving_with_pallet
     is_pallet_on_properly = False
     drive_speed_crawl = 60
@@ -420,7 +433,7 @@ def pick_up_pallet_on_ground(pallet_position) -> None:
         Crane_motor.run_target(CRANE_SPEED, GROUND_LIFT_ANGLE)
         driving_with_pallet = True
     time_to_back_out = drive_forward_stop_time -  drive_forward_time
-    distance_to_back_out = (time_to_back_out * drive_speed_crawl) /1000 #(ms *mm/s)/m
+    distance_to_back_out = (time_to_back_out * drive_speed_crawl) /1000
     distance_to_back_out = robot.distance()
     if pallet_position == 1:
         robot.straight(-distance_to_back_out)
@@ -428,30 +441,46 @@ def pick_up_pallet_on_ground(pallet_position) -> None:
         robot.straight(-distance_to_back_out * 2)
 
 def reset_crane():
+    """
+    Lifts the crane then lowers it down to ground level.
+    """
     Crane_motor.run_until_stalled(CRANE_SPEED)
     Crane_motor.run_until_stalled(-CRANE_SPEED)
 
-# Main thread for driving etc
 def main():
+    """
+    The main function that runs the whole program
+    """
     while(True):
         select_path()
         drive_to_destination()
         find_pallet(True)
         return_to_area()
-        # follow_line(colour_sensor.rgb(),COLORS["green"])
+
+def main2():
+    global driving_with_pallet
+    driving_with_pallet = True
+    return_to_circle()
+    select_path()
 
 def get_color():
+    """
+    Lets the user enter the color of the pallets the robot shall collect
+    """
     while (True):
-        global path_color
+        global selected_path_color
         color = input("Choose color... ")
         if str(color).lower() in COLORS.keys():
-            path_color = color.lower()
+            selected_path_color = color.lower()
             print(color)
             
         else:
             print("No color matching input")
 
 def collision_check():
+    """
+    Checks if there is something infront of the robot
+    """
     global clear_road
     while True:
         if (ultrasonic_sensor.distance() < STOP_DISTANCE) and (driving_with_pallet == False):
@@ -460,6 +489,9 @@ def collision_check():
             clear_road = True
 
 def check_emergency():
+    """
+    If the pallet is dropped the robot enters emergency mode and drives to the side off the road
+    """
     global driving_with_pallet
     global emergency_mode
     while True:
@@ -475,7 +507,8 @@ def check_emergency():
             print_on_screen("LETS GOOO")
             emergency_mode = False
 
-# open_file_colors(calibrate_colors(COLORS))
+# Adding color values to file for saving while also calibrating
+open_file_colors(calibrate_colors(COLORS))
 
 with open('RGB.txt') as f:
     data = f.read()
@@ -484,12 +517,10 @@ with open('RGB.txt') as f:
 COLORS = json.loads(data)
 print(COLORS)
 
-# _thread.start_new_thread(collision_check,(),)
-#_thread.start_new_thread(main,(),)
-#_thread.start_new_thread(get_color,(),)
-#_thread.start_new_thread(check_emergency,(),)
+_thread.start_new_thread(collision_check,(),)
+_thread.start_new_thread(main,(),)
+_thread.start_new_thread(get_color,(),)
+_thread.start_new_thread(check_emergency,(),)
 
 while True:
-    print(colour_sensor.rgb())
-    wait(200)
     pass
